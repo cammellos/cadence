ac170756b307c48a8bd0ec7d8fa8b21b30b9120d
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2026-05-09 13:41:26 +0100

    Add nix file

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Cadence";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      formatter = forAllSystems (system: (import nixpkgs { inherit system; }).nixfmt-rfc-style);
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              haskellPackages.ghc
+              cabal-install
+            ];
+          };
+        }
+      );
+    };
+}
